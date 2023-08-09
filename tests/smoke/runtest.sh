b82fc1268f5456a0496e3e0a8c71b73004e72126
Parag Nemade <pnemade@fedoraproject.org>
2023-08-09 19:53:47 +0530

    Updates CI tests

--- tests/smoke/runtest.sh
+++ tests/smoke/runtest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ../source
-python3 -m pytest --ignore Tests/misc/plistlib_test.py
+python3 -m pytest --ignore Tests/otlLib/optimize_test.py --ignore Tests/varLib/merger_test.py --ignore Tests/varLib/varLib_test.py
